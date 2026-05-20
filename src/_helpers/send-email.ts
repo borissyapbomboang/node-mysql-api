ee90abb1db0d60c6e9678e70ac04037d1de1b8e3
Rasheed Boriss D. Yap <borissyap105@gmail.com>
2026-05-20 11:58:49 +0800

    new email sender

--- src/_helpers/send-email.ts
+++ src/_helpers/send-email.ts
@@ -1,22 +1,8 @@
-import nodemailer from 'nodemailer';
+import { Resend } from 'resend';
 const config = require('../../config.json');
 
-export default async function sendEmail({ to, subject, html, from = config.emailFrom }: any) {
-    const transporter = nodemailer.createTransport({
-        ...config.smtpOptions,
-        connectionTimeout: 10000, // ✅ 10 seconds
-        greetingTimeout: 10000,   // ✅ 10 seconds
-        socketTimeout: 10000      // ✅ 10 seconds
-    });
-    await transporter.sendMail({ from, to, subject, html });
-}
+const resend = new Resend(config.resendApiKey);
 
-const transporter = nodemailer.createTransport({
-    ...config.smtpOptions,
-    connectionTimeout: 10000,
-    greetingTimeout: 10000,
-    socketTimeout: 10000,
-    tls: {
-        rejectUnauthorized: false // ✅ fixes SSL issues
-    }
-});
+export default async function sendEmail({ to, subject, html, from = config.emailFrom }: any) {
+    await resend.emails.send({ from, to, subject, html });
+}
